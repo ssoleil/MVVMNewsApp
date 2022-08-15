912f6478742a85de54cf425e952c4f851065fa04
ssoliel <polina.rohoza@gmail.com>
2022-08-15 22:28:25 +0300

    adding empty fragment layouts; setting up navigation component with bottom bar

--- app/src/main/java/com/example/mvvmnewsapp/UI/NewsActivity.kt
+++ app/src/main/java/com/example/mvvmnewsapp/UI/NewsActivity.kt
@@ -2,11 +2,20 @@ package com.example.mvvmnewsapp.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mvvmnewsapp.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        navBar.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
