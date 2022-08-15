2576430d2cdfb426c111bc91dd20f48c953b7956
ssoliel <polina.rohoza@gmail.com>
2022-08-15 15:42:05 +0300

    add layout xmls

--- app/src/main/java/com/example/mvvmnewsapp/UI/NewsActivity.kt
+++ app/src/main/java/com/example/mvvmnewsapp/UI/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mvvmnewsapp
+package com.example.mvvmnewsapp.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvmnewsapp.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
