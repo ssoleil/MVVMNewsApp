f0c51f84823171106769e27cd8fb76b473613137
ssoliel <polina.rohoza@gmail.com>
2022-08-16 16:31:14 +0300

    add article DAO with Room

---
+++ app/src/main/java/com/example/mvvmnewsapp/db/ArticleDao.kt
@@ -0,0 +1,17 @@
+package com.example.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.mvvmnewsapp.Article
+
+@Dao
+interface ArticleDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(article: Article) : Long
+
+    @Query("SELECT * from articles")
+    fun getAllArticles() : LiveData<List<Article>>
+
+    @Delete
+    suspend fun delete(article: Article)
+}
